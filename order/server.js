4179c61685f28d4c1a1df8c0887d95ec4e664c7f
aziztaufiqurrahman <taufiqqurrahman2899@gmail.com>
2023-03-13 15:59:03 +0700

    terjadi perbaikan pada delivery service dan order service

--- order/server.js
+++ order/server.js
@@ -2,27 +2,51 @@ const express = require("express");
 const app = express();
 
 const port = 8081;
-app.get("/order-list", (req,res)=>{
-    let response = {
-        data: {
-            item: [
-                {
-                    id: 1,
-                    name: 'order-1'
-                },
-                {
-                    id: 2,
-                    name: 'order-2'
-                }
-            ]
-        }
-    };
-    res.status(200).json(response);
-});
-app.get("/", (req,res)=>{
-    res.send("Order called");
+
+app.use(express.json());
+
+let orders = [
+  {
+    id: 1,
+    name: "Order-1",
+    status: "pending",
+  },
+  {
+    id: 2,
+    name: "Order-2",
+    status: "shipped",
+  },
+];
+
+app.get("/order-list", (req, res) => {
+  let response = {
+    data: {
+      item: orders,
+    },
+  };
+  res.status(200).json(response);
 });
 
-app.listen(port, ()=>{
-    console.log("Listening at localhost "+ port);    
-})
+app.post("/order", (req, res) => {
+  const newOrder = {
+    id: orders.length + 1,
+    name: req.body.name,
+    status: req.body.status,
+  };
+  orders.push(newOrder);
+
+  let response = {
+    data: {
+      item: orders,
+    },
+  };
+  res.status(200).json(response);
+});
+
+app.get("/", (req, res) => {
+  res.send("Order called");
+});
+
+app.listen(port, () => {
+  console.log("Listening at localhost " + port);
+});
